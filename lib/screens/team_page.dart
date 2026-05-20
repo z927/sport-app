0898ca203c1b6632adb256b8980e1cc3c27ef253
z927 <46448019+z927@users.noreply.github.com>
2026-05-20 18:01:16 +0200

    feat: remove external launcher links and keep navigation in-app

--- lib/screens/team_page.dart
+++ lib/screens/team_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import '../models/team_content.dart';
-import '../utils/link_launcher.dart';
 
 class TeamPage extends StatelessWidget {
   const TeamPage({required this.players, super.key});
@@ -24,7 +23,13 @@ class TeamPage extends StatelessWidget {
         return Card(
           child: InkWell(
             borderRadius: BorderRadius.circular(12),
-            onTap: () => openExternalUrl(player.profileUrl),
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute<void>(
+                  builder: (_) => _PlayerDetailsPage(player: player),
+                ),
+              );
+            },
             child: Padding(
               padding: const EdgeInsets.all(16),
               child: Column(
@@ -44,6 +49,30 @@ class TeamPage extends StatelessWidget {
           ),
         );
       },
+    );
+  }
+}
+
+class _PlayerDetailsPage extends StatelessWidget {
+  const _PlayerDetailsPage({required this.player});
+
+  final Player player;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(player.name)),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Numero: #${player.number}', style: Theme.of(context).textTheme.titleLarge),
+            const SizedBox(height: 12),
+            Text('Profilo backend: ${player.profileUrl}'),
+          ],
+        ),
+      ),
     );
   }
 }
