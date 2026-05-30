753b4be05b4b8d001bbea0798fdfe8feebaf0492
z927 <46448019+z927@users.noreply.github.com>
2026-05-30 17:01:59 +0200

    fix(android): fix android name 💚
    Co-authored-by: zasi <simone.zanetti.relewant@corner.ch>

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.sports_team_app"
+    namespace = "com.zasi.varese.basket.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.sports_team_app"
+        applicationId = "com.zasi.varese.basket.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/zasi/varese/basket/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/zasi/varese/basket/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.sports_team_app
+package com.zasi.varese.basket.app
 
 import io.flutter.embedding.android.FlutterActivity
 
